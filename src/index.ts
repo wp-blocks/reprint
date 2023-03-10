07f05e404b5984d9e3e4d28c35d2052a01412fd3
John Hooks <bitmachina@outlook.com>
2023-03-10 12:07:15 -0800

    add dep unist-util-visit

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
-import rehypeStringify from 'rehype-stringify'
+import { unified } from "unified";
+import rehypeParse from "rehype-parse";
+import { visit } from "unist-util-visit";
 
-main()
+main();
 
 async function main() {
-  const doc = `
+	const doc = `
   <!-- wp:paragraph -->
-  <p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+    <p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
   <!-- /wp:paragraph -->
 
   <!-- wp:paragraph {"fontSize":"medium"} -->
-  <p class="has-medium-font-size">Hello World</p>
+    <p class="has-medium-font-size">Hello World</p>
   <!-- /wp:paragraph -->
-  `
+  `;
 
-  console.log(
-    JSON.stringify(
-      unified()
-        .use(rehypeParse, {fragment: true})
-        //.use(rehypeStringify)
-        .parse(doc), null, 2
-    )
-  )
+	const tree = unified().use(rehypeParse, { fragment: true }).parse(doc);
+
+	visit(tree, "comment", (node) => {
+		console.log(node);
+	});
 }
